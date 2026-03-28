5bd88dc15eb420c5762014d6aaa8a19f204b60ec
thevortexalpha <vsvatheking@gmail.com>
2026-03-28 20:10:07 +0530

    Static dropdown handling

--- cypress/integration/MySecondTest.js
+++ cypress/integration/MySecondTest.js
@@ -4,9 +4,13 @@ describe("My Second test", () => {
 
     it("Launching application", () => {
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+        // Handling checkboxes
         cy.get("#checkBoxOption1").check().should("be.checked").and("have.value", "option1");
         cy.get("#checkBoxOption1").uncheck().should("not.be.checked");
         cy.get("input[type='checkbox']").check(["option2", "option3"]);
+
+        // Handling static dropdowns
+        cy.get("select").select("option3").should("have.value", "option3");
     });
 
 })
