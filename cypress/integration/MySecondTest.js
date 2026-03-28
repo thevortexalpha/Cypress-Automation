e82b969dd55777df2aa4f3a15e46ebbbf5e3d0d7
thevortexalpha <vsvatheking@gmail.com>
2026-03-28 20:00:43 +0530

    Checkbox interaction

---
+++ cypress/integration/MySecondTest.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe("My Second test", () => {
+
+    it("Launching application", () => {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+        cy.get("#checkBoxOption1").check().should("be.checked").and("have.value", "option1");
+        cy.get("#checkBoxOption1").uncheck().should("not.be.checked");
+        cy.get("input[type='checkbox']").check(["option2", "option3"]);
+    });
+
+})
