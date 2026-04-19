d3336539f013b658b3fcf1c05c00dc49e50c6a73
thevortexalpha <vsvatheking@gmail.com>
2026-04-19 19:06:56 +0530

    Handling web tables

---
+++ cypress/integration/CY_005_MyFifthTest.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe("My Fourth test", () => {
+
+    it("Handling web tables", async() => {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+        cy.get("tbody tr td:nth-child(2)").each(($el, index, $list) => {
+            const courseName = $el.text();
+            if(courseName.includes("SoapUI")){
+                cy.get("tbody tr td:nth-child(2)").eq(index).next().then((priceElement) => {
+                    const coursePrice = priceElement.text();
+                    expect(coursePrice).to.equal("35")
+                })
+            }
+        })
+    })
+
+});
